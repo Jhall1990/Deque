01b03df83df67f5e7c9178352a1a491d36ad2519
jhall <jhall@extremenetworks.com>
2019-01-02 23:54:25 -0500

    Converted deque back to double linked list, Implemented RandomizedQueue with array

--- Deque.java
+++ Deque.java
@@ -1,22 +1,36 @@
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+
 public class Deque<Item> implements Iterable<Item> {
-    private int head;
-    private int tail;
+
+    private TwoLinkNode head;
+    private TwoLinkNode tail;
     private int count;
-    private Item[] deque;
+
+    /*
+    Double linked node class. Keeps track of both the next
+    and previous nodes. Accepts a generic item on construction.
+     */
+    private class TwoLinkNode {
+        TwoLinkNode next;
+        TwoLinkNode prev;
+        private final Item data;
+
+        TwoLinkNode(Item i) {
+            data = i;
+        }
+    }
 
     public Deque() {
         // construct an empty deque
         count = 0;
-
-        // This is needed because we cannot create a generic array in java
-        deque = (Item[]) new Object[1];
     }
 
     public boolean isEmpty() {
-        return count == 0;
+        // Check if the deque is empty by seeing if both head and tail
+        // are null.
+        return head == null && tail == null;
     }
 
     public int size() {
@@ -27,90 +41,87 @@ public class Deque<Item> implements Iterable<Item> {
     public void addFirst(Item item) {
         // add the item to the front
         if (item == null) {
-            throw new IllegalArgumentException("item cannot be null");
+            throw new IllegalArgumentException("item must not be null");
         }
 
-        if (count == 0) {
-            deque[0] = item;
-        } else {
-            if (head == 0) {
-                resizeHead(deque.length * 2);
-            }
-            deque[--head] = item;
-        }
-
-        // Increment the count.
         count++;
+
+        TwoLinkNode newNode = new TwoLinkNode(item);
+
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            TwoLinkNode origHead = head;
+            head = newNode;
+            newNode.next = origHead;
+            origHead.prev = newNode;
+        }
     }
 
     public void addLast(Item item) {
         // add the item to the end
         if (item == null) {
-            throw new IllegalArgumentException("item cannot be null");
+            throw new IllegalArgumentException("item must not be null");
         }
 
-        if (count == 0) {
-            deque[0] = item;
-        } else {
-            if (tail + 1 == deque.length) {
-                resizeTail(deque.length * 2);
-            }
-            deque[++tail] = item;
-
-        }
-
-        // Increment the count.
         count++;
+
+        TwoLinkNode newNode = new TwoLinkNode(item);
+
+        if (tail == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            TwoLinkNode origTail = tail;
+            tail = newNode;
+            newNode.prev = origTail;
+            origTail.next = newNode;
+        }
     }
 
     public Item removeFirst() {
         // remove and return the item from the front
-        Item returnItem = deque[head];
-        deque[head] = null;
-        head++;
-        count--;
-
-        if (returnItem == null) {
-            throw new NoSuchElementException();
+        if (isEmpty()) {
+            throw new NoSuchElementException("Deque is empty");
         }
-        return returnItem;
+
+        count--;
+        TwoLinkNode origHead = head;
+        head = origHead.next;
+
+        // Check if the new head is null, If it is we know
+        // the deque is empty, set the tail to null as well.
+        if (head == null) {
+            tail = null;
+        } else {
+            // Otherwise set the new head node's prev pointer to null.
+            head.prev = null;
+        }
+
+        return origHead.data;
     }
 
     public Item removeLast() {
         // remove and return the item from the end
-        Item returnItem = deque[tail];
-        deque[tail] = null;
-        tail--;
+        if (isEmpty()) {
+            throw new NoSuchElementException("Deque is empty");
+        }
+
         count--;
+        TwoLinkNode origTail = tail;
+        tail = origTail.prev;
 
-        if (returnItem == null) {
-            throw new NoSuchElementException();
-        }
-        return returnItem;
-    }
-
-    private void resizeHead(int capacity) {
-        // Needed because you cannot create a generic array in java.
-        Item[] copy = (Item[]) new Object[capacity];
-
-        for (int i = deque.length; i > 0; i--) {
-            copy[copy.length - i] = deque[deque.length - i];
+        // Check if the new tail is null. If it is we know
+        // the deque is empty, set the head to null as well.
+        if (tail == null) {
+            head = null;
+        } else {
+            // Otherwise set the new tail node's next pointer to null.
+            tail.next = null;
         }
 
-        head = copy.length - deque.length;
-        tail = head + count - 1;
-        deque = copy;
-    }
-
-    private void resizeTail(int capacity) {
-        // Needed because you cannot create a generic array in java.
-        Item[] copy = (Item[]) new Object[capacity];
-
-        for (int i = 0; i < deque.length; i++) {
-            copy[i] = deque[i];
-        }
-
-        deque = copy;
+        return origTail.data;
     }
 
     public Iterator<Item> iterator() {
@@ -119,163 +130,21 @@ public class Deque<Item> implements Iterable<Item> {
     }
 
     private class DequeIterator implements Iterator<Item> {
-        private int current = head;
+        private TwoLinkNode current = head;
 
+        @Override
         public boolean hasNext() {
-            return deque[current] != null;
-        }
-
-        public void remove() {
-            throw new UnsupportedOperationException("remove is not supported");
+            return current != null;
         }
 
         public Item next() {
-            Item returnItem = deque[++current];
-
-            if (returnItem == null) {
-                throw new NoSuchElementException();
+            if (current == null) {
+                throw new NoSuchElementException("Deque is empty");
             }
-            return deque[++current];
+
+            Item data = current.data;
+            current = current.next;
+            return data;
         }
     }
-
-    public static void main(String[] args) {
-        Deque<Integer> d = new Deque<>();
-
-        d.addFirst(0);
-        d.addFirst(1);
-        d.addFirst(2);
-        d.addFirst(3);
-        d.addFirst(4);
-        d.addLast(-1);
-        d.addLast(-2);
-        d.addLast(-3);
-        d.addLast(-4);
-        d.addLast(-5);
-        d.addLast(-6);
-        d.addLast(-7);
-        d.addLast(-8);
-        d.addLast(-9);
-        d.addFirst(5);
-        d.addFirst(6);
-        d.addFirst(7);
-        d.addFirst(8);
-
-        System.out.println(d.removeFirst());
-        System.out.println(d.removeFirst());
-        System.out.println(d.removeFirst());
-        System.out.println(d.removeFirst());
-
-        System.out.println(d.removeLast());
-        System.out.println(d.removeLast());
-        System.out.println(d.removeLast());
-        System.out.println(d.removeLast());
-
-        System.out.println("done");
-    }
 }
-
-//import java.util.Iterator;
-//
-//public class Deque<Item> implements Iterable<Item> {
-//    private class TwoLinkNode {
-//        private Item data;
-//        TwoLinkNode next;
-//        TwoLinkNode prev;
-//
-//        TwoLinkNode(Item i) {
-//            data = i;
-//        }
-//    }
-//
-//    private TwoLinkNode head;
-//    private TwoLinkNode tail;
-//    private int count;
-//
-//    public Deque() {
-//        // construct an empty deque
-//        count = 0;
-//    }
-//
-//    public boolean isEmpty() {
-//        return head == null && tail == null;
-//    }
-//
-//    public int size() {
-//        // return the number of items on the deque
-//        return count;
-//    }
-//
-//    public void addFirst(Item item) {
-//        // add the item to the front
-//        // Increment the count.
-//        count++;
-//
-//        TwoLinkNode newNode = new TwoLinkNode(item);
-//
-//        if (head == null && tail == null) {
-//            head = newNode;
-//            tail = newNode;
-//        } else {
-//            TwoLinkNode origHead = head;
-//            head = newNode;
-//            newNode.next = origHead;
-//            origHead.prev = newNode;
-//        }
-//    }
-//
-//    public void addLast(Item item) {
-//        // add the item to the end
-//        // Decrement the count.
-//        count++;
-//
-//        TwoLinkNode newNode = new TwoLinkNode(item);
-//
-//        if (head == null && tail == null) {
-//            head = newNode;
-//            tail = newNode;
-//        } else {
-//            TwoLinkNode origTail = tail;
-//            tail = newNode;
-//            newNode.prev = origTail;
-//            origTail.next = newNode;
-//        }
-//    }
-//
-//    public Item removeFirst() {
-//        // remove and return the item from the front
-//        // Move the first pointer to the original first item's next pointer.
-//        count--;
-//        TwoLinkNode origHead = head;
-//        head = origHead.prev;
-//        return origHead.data;
-//    }
-//
-//    public Item removeLast() {
-//        // remove and return the item from the end
-//        count--;
-//        TwoLinkNode origTail = tail;
-//        tail = origTail.next;
-//        return origTail.data;
-//    }
-//
-//    public Iterator<Item> iterator() {
-//        // return an iterator over items in order from front to end
-//        return new DequeIterator();
-//    }
-//
-//    private class DequeIterator implements Iterator<Item> {
-//        private TwoLinkNode current = head;
-//
-//        @Override
-//        public boolean hasNext() {
-//            return current != null;
-//        }
-//
-//        public Item next() {
-//            Item data = current.data;
-//            current = current.next;
-//            return data;
-//        }
-//    }
-//}
--- RandomizedQueue.java
+++ RandomizedQueue.java
@@ -1,32 +1,73 @@
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Random;
+
+import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
+    private int count;
+    private Item[] randomDeque;
+
     public RandomizedQueue() {
         // construct an empty randomized queue
+        count = 0;
+        randomDeque = (Item[]) new Object[1];
     }
 
     public boolean isEmpty() {
         // is the randomized queue empty?
-        return true;
+        return count == 0;
     }
 
     public int size() {
         // return the number of items on the randomized queue
-        return 0;
+        return count;
     }
 
     public void enqueue(Item item) {
         // add the item
+        if (item == null) {
+            throw new IllegalArgumentException("item must not be null");
+        }
+
+        randomDeque[count++] = item;
+
+        if (count == randomDeque.length) {
+            grow(2 * count);
+        }
     }
 
     public Item dequeue() {
         // remove and return a random item
-        return null;
+        if (size() == 0) {
+            throw new NoSuchElementException("Deque is empty");
+        }
+
+        int randInt = StdRandom.uniform(count);
+        Item returnItem = randomDeque[randInt];
+        randomDeque[randInt] = randomDeque[count - 1];
+        randomDeque[--count] = null;
+        return returnItem;
     }
 
     public Item sample() {
         // return a random item (but do not remove it)
-        return null;
+        if (size() == 0) {
+            throw new NoSuchElementException("Deque is empty");
+        }
+
+        int randInt = StdRandom.uniform(0, count);
+        return randomDeque[randInt];
+    }
+
+    private void grow(int capicity) {
+        Item[] copy = (Item[]) new Object[capicity];
+
+        for (int i = 0; i < randomDeque.length; i++) {
+            copy[i] = randomDeque[i];
+        }
+
+        randomDeque = copy;
     }
 
     public Iterator<Item> iterator() {
@@ -35,17 +76,59 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     }
 
     private class RandomizedQueueIterator implements Iterator<Item> {
-        @Override
+        Item[] randomQueueCopy;
+        int index;
+
+        RandomizedQueueIterator() {
+            randomQueueCopy = (Item[]) new Object[count];
+
+            for (int i = 0; i < count; i++) {
+                int randIndex = StdRandom.uniform(count);
+                while (randomQueueCopy[randIndex] != null) {
+                    randIndex = StdRandom.uniform(count);
+                }
+                randomQueueCopy[randIndex] = randomDeque[randIndex];
+            }
+            index = 0;
+        }
+
         public boolean hasNext() {
-            return false;
+            return index != count;
+
         }
 
         public Item next() {
-            return null;
+            if (index == count) {
+                throw new NoSuchElementException("no more elements");
+            }
+
+            return randomQueueCopy[index++];
         }
     }
 
     public static void main(String[] args) {
-        // unit testing (optional)
+        RandomizedQueue<Integer> d = new RandomizedQueue<>();
+
+        d.enqueue(0);
+        d.enqueue(1);
+        d.enqueue(2);
+        d.enqueue(3);
+        d.enqueue(4);
+        d.enqueue(5);
+        d.enqueue(6);
+        d.enqueue(7);
+        d.enqueue(8);
+        d.enqueue(9);
+
+        Iterator<Integer> i1 = d.iterator();
+        Iterator<Integer> i2 = d.iterator();
+
+        while (i1.hasNext()) {
+            System.out.println("Iterator it " + i1.next());
+        }
+
+        while (i2.hasNext()) {
+            System.out.println("Iterator it2 " + i2.next());
+        }
     }
 }
